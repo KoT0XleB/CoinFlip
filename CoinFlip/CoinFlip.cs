562aef7b80a9ad436b38ceef2f9f335c1b6d508a
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2021-12-28 12:52:51 +0500

    Add files via upload

---
+++ CoinFlip/CoinFlip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Qurre;
+using MEC;
+using Qurre.API.Events;
+
+namespace CoinFlip
+{
+    public class CoinFlip : Plugin
+    {
+        public override string Developer => "KoToXleB#4663";
+        public override string Name => "CoinFlip";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public static Config CustomConfig { get; private set; }
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.CoinFlip += OnCoinFlip;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.CoinFlip -= OnCoinFlip;
+        }
+        public static void OnCoinFlip(CoinFlipEvent ev)
+        {
+            Timing.CallDelayed(CustomConfig.Delay, () =>
+            {
+                if (UnityEngine.Random.Range(0, 2) == 0) ev.Player.ShowHint($"{CustomConfig.Result}: {CustomConfig.FirstCoin}", CustomConfig.DelayBroadcast);
+                else ev.Player.ShowHint($"{CustomConfig.Result}: {CustomConfig.SecondCoin}", CustomConfig.DelayBroadcast);
+            });
+        }
+    }
+}
---
+++ CoinFlip/Config.cs
@@ -0,0 +1,24 @@
+﻿using Qurre.API.Addons;
+using System.ComponentModel;
+
+namespace CoinFlip
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "CoinFlip";
+
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("Just result string")]
+        public string Result { get; set; } = "Result ";
+        [Description("The name of the first side of the coin? Tail or Решка")]
+        public string FirstCoin { get; set; } = "Tail";
+        [Description("The name of the second side of the coin? Head or Орел")]
+        public string SecondCoin { get; set; } = "Head";
+        [Description("Delay before showing broadcast")]
+        public float Delay { get; set; } = 1.5f;
+        [Description("How long should the broadcast show?")]
+        public float DelayBroadcast { get; set; } = 3f;
+    }
+}
