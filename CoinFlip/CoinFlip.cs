af3c2fd413fcd028aea7ea9dafc398d614d935d9
KoT0XleB <lapka.roman@yandex.ru>
2022-06-27 22:15:09 +0500

    2.0
    The end is never end

--- CoinFlip/CoinFlip.cs
+++ CoinFlip/CoinFlip.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using Qurre;
 using MEC;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Objects;
 using Qurre.API.Events;
+using UnityEngine;
 
 namespace CoinFlip
 {
@@ -9,7 +12,7 @@ namespace CoinFlip
     {
         public override string Developer => "KoT0XleB#4663";
         public override string Name => "CoinFlip";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(2, 0, 0);
         public override int Priority => int.MinValue;
         public override void Enable() => RegisterEvents();
         public override void Disable() => UnregisterEvents();
@@ -20,22 +23,72 @@ namespace CoinFlip
             CustomConfigs.Add(CustomConfig);
             if (!CustomConfig.IsEnable) return;
 
-            Qurre.Events.Player.CoinFlip += OnCoinFlip;
+            if (CustomConfig.TeleportEnable) Qurre.Events.Player.CoinFlip += OnCoinFlip;
+            if (CustomConfig.FlipMessageEnable) Qurre.Events.Player.PickupItem += OnCoinPickup;
         }
         public void UnregisterEvents()
         {
             CustomConfigs.Remove(CustomConfig);
             if (!CustomConfig.IsEnable) return;
 
-            Qurre.Events.Player.CoinFlip -= OnCoinFlip;
+            if (CustomConfig.TeleportEnable) Qurre.Events.Player.CoinFlip -= OnCoinFlip;
+            if (CustomConfig.FlipMessageEnable) Qurre.Events.Player.PickupItem -= OnCoinPickup;
         }
         public void OnCoinFlip(CoinFlipEvent ev)
         {
             Timing.CallDelayed(CustomConfig.Delay, () =>
             {
-                if (ev.Tails) ev.Player.ShowHint(CustomConfig.Result + CustomConfig.FirstCoin, CustomConfig.DelayBroadcast);
-                else ev.Player.ShowHint(CustomConfig.Result + CustomConfig.SecondCoin, CustomConfig.DelayBroadcast);
+                var roomNumber = UnityEngine.Random.Range(1, 50);
+                switch (roomNumber)
+                {
+                    // bugs-dangerous rooms
+                    case 12:
+                    case 42:
+                    case 48:
+                    case 49: break;
+                    // Dangerous rooms
+                    case 20:
+                    case 28: break;
+                    // Normal rooms
+                    default: break;
+                }
+                if (ev.Player.ItemTypeInHand == ItemType.Coin)
+                {
+                    if (Qurre.API.Controllers.Alpha.Detonated == false)
+                    {
+                        if (roomNumber == 12) ev.Player.TeleportToDoor(DoorType.Nuke_Armory);
+                        else if (roomNumber == 42) ev.Player.TeleportToDoor(DoorType.HCZ_049_Gate);
+                        else if (roomNumber == 48) ev.Player.TeleportToDoor(DoorType.HID);
+                        else if (roomNumber == 49)
+                        {
+                            ev.Player.TeleportToDoor(DoorType.Surface_Nuke);
+                            ev.Player.ShowHint(CustomConfig.LuckyMessage, 5);
+                        }
+                        else ev.Player.Position = Extensions.GetRoom((RoomType)roomNumber).Position + Vector3.up * 2;
+                    }
+                    else
+                    {
+                        switch (UnityEngine.Random.Range(1, 4))
+                        {
+                            case 1: ev.Player.Position = new Vector3(182.57f, 993.91f, -85.54f); break;
+                            case 2: ev.Player.Position = new Vector3(40.51f, 988.68f, -35.23f); break;
+                            case 3: ev.Player.Position = new Vector3(-10.9f, 1001.15f, -21.17f); break;
+                        }
+                    }
+                    ev.Player.RemoveItem(ev.Player.ItemInHand);
+                    ev.Player.ClearBroadcasts();
+                    ev.Player.Broadcast(CustomConfig.CoinTeleportMessage, 3);
+                }
+                ev.Player.ShowHint(CustomConfig.Result + CustomConfig.FirstCoin, CustomConfig.DelayBroadcast);
             });
+        }
+        public void OnCoinPickup(PickupItemEvent ev)
+        {
+            if (ev.Pickup.Type == ItemType.Coin)
+            {
+                ev.Player.ClearBroadcasts();
+                ev.Player.Broadcast(CustomConfig.PickupCoinTeleport, 5);
+            }
         }
     }
 }
--- CoinFlip/Config.cs
+++ CoinFlip/Config.cs
@@ -10,12 +10,22 @@ namespace CoinFlip
 
         [Description("Enable the plugin?")]
         public bool IsEnable { get; set; } = true;
+        [Description("Enable the ability to teleport when flip a coin?")]
+        public bool TeleportEnable { get; set; } = true;
+        [Description("Enable the coin-side text?")]
+        public bool FlipMessageEnable { get; set; } = true;
         [Description("Just result string. Result: ")]
         public string Result { get; set; } = "Результат: ";
         [Description("The name of the first side of the coin? Tail or Решка")]
         public string FirstCoin { get; set; } = "Решка";
         [Description("The name of the second side of the coin? Head or Орел")]
         public string SecondCoin { get; set; } = "Орел";
+        [Description("A message when a player is lucky enough to be on the street while teleporting.")]
+        public string LuckyMessage { get; set; } = "<color=green><b>Чувак</b>, тебе повезло!</color>";
+        [Description("A message when a person was teleported by a coin.")]
+        public string CoinTeleportMessage { get; set; } = "<color=red>Вас телепортировала <color=cyan>монетка!</color></color>";
+        [Description("A message when a person picks up a coin.")]
+        public string PickupCoinTeleport { get; set; } = "<color=yellow>Вы подобрали <color=#77DD77>Монетку Телепортации.</color>\n Подкиньте монетку, чтобы <color=#990066>телепортироваться.</color></color>";
         [Description("Delay before showing broadcast")]
         public float Delay { get; set; } = 1.5f;
         [Description("How long should the broadcast show?")]
